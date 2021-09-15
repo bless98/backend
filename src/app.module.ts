2060a3809cb8880b2e4de1a312a3632d8566f986
Hoyozani Chabalala <chabalala@thedigitalacademy.co.za>
2021-09-15 11:39:29 +0200

    renaming database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PhonebookModule } from './phonebook/phonebook.module';
 
+
 @Module({
 	imports: [
-		MongooseModule.forRoot("mongodb://localhost:27017/PHONEBOOK_DB", { autoCreate: true }),
+		MongooseModule.forRoot("mongodb+srv://Hoyozani:bless@98@cluster0.fa5wj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { autoCreate: true }),
 		PhonebookModule,
 	],
 	controllers: [],
