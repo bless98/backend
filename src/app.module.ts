d139f5b876f69ee4110f4985714b52027c5fb663
Hoyozani Chabalala <chabalala@thedigitalacademy.co.za>
2021-09-15 11:56:03 +0200

    connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { PhonebookModule } from './phonebook/phonebook.module';
 
 @Module({
 	imports: [
-		MongooseModule.forRoot("mongodb+srv://Hoyozani:bless@98@cluster0.fa5wj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { autoCreate: true }),
+		MongooseModule.forRoot("mongodb+srv://Hoyozani:chabalalahb@cluster0.fa5wj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { autoCreate: true }),
 		PhonebookModule,
 	],
 	controllers: [],
